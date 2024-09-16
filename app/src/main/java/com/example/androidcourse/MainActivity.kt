bb2cfb429bf1b1c0f1106bed86096e0e36e29dd8
AndroidDeveloper3005 <androdevhimel005@gmail.com>
2024-09-16 13:46:11 +0600

    button and edittext done

--- app/src/main/java/com/example/androidcourse/MainActivity.kt
+++ app/src/main/java/com/example/androidcourse/MainActivity.kt
@@ -2,26 +2,31 @@ package com.example.androidcourse
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-
     //variable
     //var wellcomeTv : TextView? = null
     lateinit var wellcomeTv : TextView    // two type of solve   samone lateinit
+    lateinit var nameEt : EditText
+    lateinit var showBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         wellcomeTv = findViewById(R.id.textTv)
-        wellcomeTv.setText("Hi")
+        nameEt = findViewById(R.id.editTextText)
+        showBtn = findViewById(R.id.button)
 
-        wellcomeTv.setOnClickListener{
-            wellcomeTv.setTextColor(Color.RED)
 
+        showBtn.setOnClickListener {
+            var result = nameEt.getText().toString()
+            wellcomeTv.setText(result).toString()
 
         }
 
