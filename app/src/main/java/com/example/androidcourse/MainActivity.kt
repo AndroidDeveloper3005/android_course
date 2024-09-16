c4727b578bce8e1ebd42f9cec203a310b5c9e248
AndroidDeveloper3005 <androdevhimel005@gmail.com>
2024-09-16 13:35:39 +0600

    text view

--- app/src/main/java/com/example/androidcourse/MainActivity.kt
+++ app/src/main/java/com/example/androidcourse/MainActivity.kt
@@ -8,9 +8,22 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    //variable
+    //var wellcomeTv : TextView? = null
+    lateinit var wellcomeTv : TextView    // two type of solve   samone lateinit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        wellcomeTv = findViewById(R.id.textTv)
+        wellcomeTv.setText("Hi")
+
+        wellcomeTv.setOnClickListener{
+            wellcomeTv.setTextColor(Color.RED)
+
+
+        }
 
     }
 }
